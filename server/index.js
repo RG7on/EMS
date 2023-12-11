b4055ab77b4e2df663f0d3ab9933d2f5ddbeccfe
Almuntaser <s26s2020@nct.edu.om>
2023-12-11 14:47:04 +0400

    registration form modification (up and runing)

--- server/index.js
+++ server/index.js
@@ -1,21 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
-const app = express();
+import cors from 'cors';
+import model from './Models/User.js';
 
-const conString="mongodb+srv://s26s2053:csse3101@systemusers.hcpchwp.mongodb.net/EMS?retryWrites=true&w=majority"
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const conString = "mongodb+srv://s26s2053:csse3101@systemusers.hcpchwp.mongodb.net/EMS?retryWrites=true&w=majority";
 mongoose.connect(conString)
-app.listen(3001,()=>{
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+    
+app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
 
-app.post("/addEmp",async(req,res)=> {
+app.post("/addEmp", async (req, res) => {
     const Emp = new model({
-        empName:req.body.empName,
-        empId:req.body.empId,
-        password:req.body.password
-    })
+        empName: req.body.empName,
+        empId: req.body.empId,
+        password: req.body.password
+    });
 
-    await Emp.save()
-    res.send('Document saved successfully')
-
-})
+    try {
+        await Emp.save();
+        res.send('Document saved successfully');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
