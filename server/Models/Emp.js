f3579d003943efd90a7f271219a927f3b7adea19
Mohad009 <26S2053@utas.edu.om>
2023-12-17 22:27:50 +0400

    employees database schema created

---
+++ server/Models/Emp.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const empSchema=new mongoose.Schema({
+    empId:{type:String,required:true},
+    firstName:{type:String,required:true},
+    lastName:{type:String,required:true},
+    gender:{type:String,required:true},
+    position:{type:String,required:true},
+    dept:{type:String,required:true},
+    contact:{
+        email:{type:String,required:true},
+        phone:{type:String,required:true}
+    },
+    address:{type:String,required:true},
+    hireDate:{type:String,required:true},
+    emergencyNumber:{type:String,required:false}
+})
+
+const empModel=mongoose.model("Employees",empSchema);
+export default empModel;
